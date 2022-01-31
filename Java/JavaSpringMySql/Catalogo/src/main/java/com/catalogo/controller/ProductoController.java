5f8b5fdb2ab2aad5d70a8b6aa1fe92bbcd2c8f65
NahuelBarrios <retro_io@hotmail.com>
2022-01-31 16:37:48 -0300

    Crud de la entidad Producto Completo

--- Java/JavaSpringMySql/Catalogo/src/main/java/com/catalogo/controller/ProductoController.java
+++ Java/JavaSpringMySql/Catalogo/src/main/java/com/catalogo/controller/ProductoController.java
@@ -17,7 +17,7 @@ public class ProductoController {
     @Autowired
     private ProductoServicio servicioP;
 
-    @PostMapping("/crearProdcuto")
+    @PostMapping("/crearProducto")
     public ResponseEntity<GenericResponse> agregar(@RequestBody ProductoDTO productoDTO, BindingResult results) {
 
         GenericResponse rta = new GenericResponse();
@@ -36,6 +36,53 @@ public class ProductoController {
             
             return ResponseEntity.ok(rta);        
         }
-
     }
+    
+    @PutMapping("/editarProducto/{idProducto}")
+    public ResponseEntity<GenericResponse> editar(@RequestBody ProductoDTO productoDTO,@PathVariable Long idProducto)
+    {
+        GenericResponse rta = new GenericResponse();
+        
+        Producto producto = servicioP.buscarIdProducto(idProducto);
+        
+        if(producto.getIdProducto() != null)
+        {
+            producto.setImagenProducto(productoDTO.getImagenProducto());
+            producto.setNombreProducto(productoDTO.getNombreProducto());
+            producto.setPrecioProducto(productoDTO.getPrecioProducto());
+            producto.setCreationDate(productoDTO.getCreationDate());
+            
+            rta.isOk = true;
+            rta.message = "El producto se edito correctamente";
+            servicioP.guardar(producto);
+            return ResponseEntity.ok(rta);
+        }
+        else{
+            rta.isOk = false;
+            rta.message = "El producto no se edito correctamente, ya que el id ingresado no existe";
+            return ResponseEntity.badRequest().body(rta);
+        }
+    }
+    
+    @DeleteMapping("/eliminarProducto/{idProducto}")
+    public ResponseEntity<GenericResponse> eliminar(@PathVariable Long idProducto)
+    {
+        GenericResponse rta = new GenericResponse();
+        
+        Producto producto = servicioP.buscarIdProducto(idProducto);
+        
+        if(producto.getIdProducto() != null)
+        {
+            rta.isOk = true;
+            rta.message = "El producto se elimino correctamente";
+            servicioP.borrar(producto);
+            return ResponseEntity.ok(rta);
+        }
+        else{
+            rta.isOk = false;
+            rta.message = "El producto no se elimino correctamente, ya que el id ingresado no existe";
+            return ResponseEntity.badRequest().body(rta);
+        }
+    }
+    
 }
